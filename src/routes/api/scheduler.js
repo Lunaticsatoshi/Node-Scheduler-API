16a309b3f8b952b1c7aecb4fb2c3e80d0ccb2812
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-30 20:57:07 +0530

    :railway_track: Adding schedule event controller to routes

--- src/routes/api/scheduler.js
+++ src/routes/api/scheduler.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
 
+import { scheduleEvent } from "../controllers/scheduler.js";
+
 export const router = Router();
 
-router.post('/schedule');
+
+// @desc     Create task sc
+// @route    POST /api/v1/schedule/create
+// @access   Public
+// @method  scheduleEvent
+router.post('/schedule', scheduleEvent);

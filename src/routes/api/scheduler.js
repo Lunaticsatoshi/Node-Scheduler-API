e786b1268becf3adc84c28f0650792bd2fd03689
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-30 21:07:46 +0530

    :railway_track: Adding test route

--- src/routes/api/scheduler.js
+++ src/routes/api/scheduler.js
@@ -4,9 +4,13 @@ import { scheduleEvent } from "../../controllers/scheduler.js";
 
 export const router = Router();
 
+router.get('/schedule/test', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+})
+
 
 // @desc     Create task sc
 // @route    POST /api/v1/schedule/create
 // @access   Public
 // @method  scheduleEvent
-router.post('/schedule', scheduleEvent);
+router.post('/schedule/create', scheduleEvent);
